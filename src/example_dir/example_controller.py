967efe8d423f23afce1f68ef4d4fff51b2dc7d01
Tutmirleid <valeriyjobemail@gmail.com>
2024-04-17 15:32:43 +0300

    added watchdog

--- src/example_dir/example_controller.py
+++ src/example_dir/example_controller.py
@@ -1,11 +1,13 @@
 from aiogram.types import Message
 
-from src.example_dir.example_service import ExampleService
+
+# from src.example_dir.example_service import ExampleService
 
 
 class ExampleController:
     def __init__(self):
-        self.example_service: ExampleService = ExampleService()
+        ...
+        # self.example_service: ExampleService = ExampleService()
 
     async def example_func(self, msg: Message):
         await msg.answer("example_msg")
