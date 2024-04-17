2bcfe40272ef7ed68793f36545255b4416a18e2b
Tutmirleid <valeriyjobemail@gmail.com>
2024-04-17 14:29:33 +0300

    added watchdog

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
 import os
+import sys
 
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from watchdog.events import LoggingEventHandler
+from watchdog.observers import Observer
 
 from src.example_dir import example_router
 
@@ -25,10 +28,17 @@ class Main:
         # Init logging
         logging.basicConfig(
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
             level=logging.INFO
         )
         logging.getLogger("httpx").setLevel(logging.WARNING)
 
+        path = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+        event_handler = LoggingEventHandler()
+        observer = Observer()
+        observer.schedule(event_handler, path, recursive=True)
+        observer.start()
         # Get routers
         self.get_routers()
 
