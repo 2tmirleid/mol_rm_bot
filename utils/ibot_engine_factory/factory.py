2c43cbcbd9a5fbfb92a0d7d5d88fb9d6cd6c2164
Tutmirleid <valeriyjobemail@gmail.com>
2024-04-19 14:33:53 +0300

    updated IBotEngineFactory

--- utils/ibot_engine_factory/factory.py
+++ utils/ibot_engine_factory/factory.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher
 
+from aiogram.fsm.storage.memory import MemoryStorage
+
 from utils.logger import Logger
 from utils.watcher import Watcher
 
@@ -9,6 +11,8 @@ class IBotEngineFactory:
                  token: str,
                  routers: list,
                  ) -> None:
+        self.storage = MemoryStorage()
+
         self.token = token
         self.bot = Bot(token=self.token)
         self.dp = Dispatcher(bot=self.bot)
@@ -16,7 +20,7 @@ class IBotEngineFactory:
         self.logger = Logger()
         self.watcher = Watcher()
 
-        self.routers = routers
+        self.routers: list = routers
 
     async def launch(self):
         if self.routers is not None:
